1ad11003aa379545fd30362ce6ff12f3088a9c8e
Santi <canosantiago404@gmail.com>
2024-08-29 17:33:58 -0300

    all rides page

--- app/(root)/(tabs)/rides.tsx
+++ app/(root)/(tabs)/rides.tsx
@@ -1,10 +1,54 @@
-import { Text } from "react-native";
+import { useUser } from "@clerk/clerk-expo";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { useFetch } from "@/lib/fetch";
+import { Ride } from "@/types/type";
+import { images } from "@/constants";
+
+import RideCard from "@/components/RideCard";
+
 const Rides = () => {
+  const { user } = useUser();
+  const { data: recentRides, loading } = useFetch<Ride[]>(
+    `/(api)/ride/${user?.id}`
+  );
+
   return (
     <SafeAreaView>
-      <Text>Rides</Text>
+      <FlatList
+        data={recentRides}
+        renderItem={({ item }) => <RideCard ride={item} />}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!loading ? (
+              <>
+                <Image
+                  source={images.noResult}
+                  className="w-40 h-40"
+                  alt="No se encontraron viajes recientes"
+                  resizeMode="contain"
+                />
+                <Text className="text-sm">No se encontraron viajes</Text>
+              </>
+            ) : (
+              <ActivityIndicator size="small" color="#000" />
+            )}
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <Text className="text-2xl font-JakartaBold my-5">
+              Todo los viajes
+            </Text>
+          </>
+        )}
+      />
     </SafeAreaView>
   );
 };
